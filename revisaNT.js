36bc916c50deb65f01eef1d1f1ffa8e4967853a8
Ratitigast <escrim.saiber@hotmail.com>
2019-08-05 12:55:06 -0500

    Se intenta obtener elementos únicos de ls_prod1

--- revisaNT.js
+++ revisaNT.js
@@ -4,6 +4,7 @@ function revisaNT(){
   this.ls_nt1=new Array(); //Lista NT1 en la que se guardarán los NT que cumplan con tener una regla de únicamente Terminales
   this.ls_prod1=new Array(); //Lista P1 en la que se guardarán los símbolos Terminales que se identificaron
                             //JUNTO CON El NÚMERO DE NT AL QUE PERTENECE
+  this.ls_prod1_clear=new Array();
 
   for(var i=0;i<this.ls_prod.length;i++){ //El límite es el tamaño de NT que generan producciones
     for(var j=0;j<this.ls_prod[i].length;j++){ //El límite es el tamaño de cada producción
@@ -13,12 +14,14 @@ function revisaNT(){
 
       if(notlowercase==lowercased){ // Si el contenido son minúsculas, identificamos una regla con únicamente TERMINALES
         this.ls_nt1[i]=this.ls_nt[i];
-        this.ls_prod1.push([this.ls_prod[i][j],i]);}
+        this.ls_prod1[i]=[this.ls_prod[i][j],i];}
     }
+    this.ls_prod1_clear[i] = this.ls_prod1.split('').filter(function(item, i, ar){ return ar.indexOf(item) === i; }).join('');
   }
 
   this.ls_nt1=this.ls_nt1.filter(Boolean); //Eliminamos vacíos, 0, undefined,...
 
+
   document.getElementById("demo3").innerHTML = "Lista de NT1: "+ this.ls_nt1;
-  document.getElementById("demo4").innerHTML = "Lista de P1: "+ this.ls_prod1;
+  document.getElementById("demo4").innerHTML = "Lista de P1: "+ this.ls_prod1_clear;
 }
