2309f0ef274d3eaa38df3eae01d73e796f893659
Ratitigast <escrim.saiber@hotmail.com>
2019-08-07 02:10:47 -0500

    Se corrigió la lista de P1 así como de los simbolos terminales

--- revisaNT.js
+++ revisaNT.js
@@ -3,8 +3,9 @@ function revisaNT(){
 //de las reglas de la produccion CONTIENE ÚNICAMENTE TERMINALES.
   this.ls_nt1=new Array(); //Lista NT1 en la que se guardarán los NT que cumplan con tener una regla de únicamente Terminales
   this.ls_prod1=new Array(); //Lista P1 en la que se guardarán los símbolos Terminales que se identificaron
-                            //JUNTO CON El NÚMERO DE NT AL QUE PERTENECE
-  this.ls_prod1_clear=new Array();
+                            //por cada NT1
+  var aux=0;
+  var ls_aux=new Array();//Guardará las reglas conformadas únicamente por terminales
 
   for(var i=0;i<this.ls_prod.length;i++){ //El límite es el tamaño de NT que generan producciones
     for(var j=0;j<this.ls_prod[i].length;j++){ //El límite es el tamaño de cada producción
@@ -14,14 +15,21 @@ function revisaNT(){
 
       if(notlowercase==lowercased){ // Si el contenido son minúsculas, identificamos una regla con únicamente TERMINALES
         this.ls_nt1[i]=this.ls_nt[i];
-        this.ls_prod1[i]=[this.ls_prod[i][j],i];}
+        ls_aux[aux]=this.ls_prod[i][j];
+        aux++;}
     }
-    this.ls_prod1_clear[i] = this.ls_prod1.split('').filter(function(item, i, ar){ return ar.indexOf(item) === i; }).join('');
+    this.ls_prod1[i]=ls_aux;
+    ls_aux=[]; //Vaciamos el arreglo para que guarde las nuevas reglas de la siguiente produccion
   }
 
   this.ls_nt1=this.ls_nt1.filter(Boolean); //Eliminamos vacíos, 0, undefined,...
-
+  this.ls_prod1= this.ls_prod1.map(it => it.filter(_ => true)).filter(sub => sub.length);
 
   document.getElementById("demo3").innerHTML = "Lista de NT1: "+ this.ls_nt1;
-  document.getElementById("demo4").innerHTML = "Lista de P1: "+ this.ls_prod1_clear;
+  document.getElementById("demo4").innerHTML = "Lista de T1: "+ this.ls_prod1;
+  //Mostramos la lista P1
+  let newArr = this.ls_prod1.map((v, i) => v.splice(0, 0, this.ls_nt1[i],':=') && v)
+  var newtope = document.createElement("P");
+  newtope.innerHTML="Lista P1 "+newArr;
+  document.getElementById("demo4").appendChild(newtope);
 }
