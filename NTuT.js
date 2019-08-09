83fb0c0d04186ff91227581e181024f55e6018e6
Ratitigast <escrim.saiber@hotmail.com>
2019-08-09 12:53:49 -0500

    Se agrega función NTuT
    Se intenta hacer la comparación NTuT con P

---
+++ NTuT.js
@@ -0,0 +1,12 @@
+function NTuT(){
+
+  for(var i=0;i<this.ls_prod.length;i++){
+    for(var j=0;j<this.ls_prod[i].length;j++){
+      p=this.ls_prod[i][j].toString();
+      ntut=newArr.toString().split();
+      
+    }
+  }
+
+
+}
