bcdbe6888698070d40a11400fe37c47f5a44622d
Ratitigast <escrim.saiber@hotmail.com>
2019-09-23 12:54:56 -0500

    Se busca la manera de crear una expreción regular para detectar los T

--- DetectaT.js
+++ DetectaT.js
@@ -1,9 +1,26 @@
 function DetectaT(str) {
+  var arr_res = new Array();
+  str_lc=arr_toStrn(str);
+  str_spcial=str_lc;
+
+  res=str_lc.replace(/^\W/,'');
+  res=res.split("");
+  arr_res[0]=res.filter(onlyUnique);
+  /*
+  res_special=str_spcial.replace(/[!@#$%^&*()_+{};':"|,.<>?]/g,'');
+  res_special=res_special.split("");
+  arr_res[1]=res_special.filter(onlyUnique);*/
+
+  full_res = arr_toStrn(arr_res);
+
+  return full_res;
+}
+function arr_toStrn(str){
+
   str=str.join();
   str=str.split(",")
   str=str.toString();
 
-  res=str.replace(/[^a-z]/g,'');
+  return str;
 
-  return res;
 }
